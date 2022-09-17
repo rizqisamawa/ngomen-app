237cacd37b8520e232ed1f021e92231072dc81ca
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-18 03:55:09 +0700

    ctxt: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="flex items-center bg-pink-600 h-[50px] p-[10px] justify-between text-white">
       <span className="font-bold">N'gomeng</span>
       <div className="flex gap-[10px] items-center">
         <img
-          src="https://images.pexels.com/photos/11739206/pexels-photo-11739206.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt="ava"
+          src={currentUser.photoURL}
+          alt="profile"
           className="bg-[#ddddf7] h-6 w-6 rounded-[50%] object-cover"
         />
-        <span className="font-thin">Maun</span>
-        <button className="bg-pink-500 text-xs rounded-md p-1">logout</button>
+        <span className="font-thin">{currentUser.displayName}</span>
+        <button
+          onClick={() => signOut(auth)}
+          className="bg-pink-500 text-xs rounded-md p-1"
+        >
+          logout
+        </button>
       </div>
     </div>
   );
