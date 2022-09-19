cc66f17fb0ad411f0aa61a66aab17d120b4841b9
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-19 23:58:04 +0700

    fetch: message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className="message flex gap-5 mb-5 flex-row-reverse">
-      <div className="info flex flex-col text-gray-500 font-light">
+    <div
+      ref={ref}
+      className={`flex gap-5 mb-5 flex-row-reverse ${
+        message.senderId === currentUser.uid && "owner"
+      }`}
+    >
+      <div className="flex flex-col text-gray-500 font-light">
         <img
-          src="https://images.pexels.com/photos/11739206/pexels-photo-11739206.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
           alt=""
           className="w-10 h-10 rounded-[50%] object-cover"
         />
         <span className="text-xs">just now</span>
       </div>
-      <div className="content max-w-[80%] flex flex-col gap-[10px] items-end">
+      <div className="max-w-[80%] flex flex-col gap-[10px] items-end">
         <p className="bg-pink-500 text-white py-[10px] px-5 rounded-tr-lg rounded-br-lg rounded-bl-lg max-w-max">
-          hello
+          {message.text}
         </p>
-        <img
-          src="https://images.pexels.com/photos/11739206/pexels-photo-11739206.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          className="w-2/4"
-        />
+
+        {message.img && <img src={message.img} alt="" className="w-2/4" />}
       </div>
     </div>
   );
