0cbf136942835f997ba3a8972f175eee85ebbb66
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-19 23:40:46 +0700

    fetch: messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,20 +1,31 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext.jsx";
+import { db } from "../firebase.jsx";
 import Message from "./Message.jsx";
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+
   return (
     <div
       className="messages bg-pink-50 p-[10px] overflow-y-scroll"
       style={{ height: "calc(100% - 100px)" }}
     >
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
